cb1770fead01cc5811547a1cfe05ccab992711c8
marsfrolova <115263855+marsfrolova@users.noreply.github.com>
2022-11-04 23:38:42 +0200

    home work

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true, 
+    margin:0, 
+    nav:true, 
+    navText: ['', ' ' ],
+    autoplay: true,
+    autoplayTimeout:2000,
+    responsive: {
+        1:{
+    items:2
+    },
+
+    1000:{
+    items:5
+    }
+}
+  })
+
